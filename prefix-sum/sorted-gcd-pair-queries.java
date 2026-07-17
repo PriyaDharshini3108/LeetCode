2362cca6045ee3b23c488356a3e90c332304c0f5
Priyadharshini M <murugesan1051974@gmail.com>
2026-07-17 21:47:20 +0530

    LC-sorted-gcd-pair-queries [Runtime: N/A; Memory: N/A]

--- prefix-sum/sorted-gcd-pair-queries.java
+++ prefix-sum/sorted-gcd-pair-queries.java
@@ -1,20 +1,36 @@
 class Solution {
-    public boolean isValidSerialization(String preorder) {
-        String[] nodes = preorder.split(",");
-        int slots = 1;
+    public int[] gcdValues(int[] A, long[] queries) {
+        int max = Arrays.stream(A).max().getAsInt();
 
-        for (String node : nodes) {
-            if (slots == 0) {
-                return false;
+        int[] freq = new int[max + 1];
+        long[] GCD = new long[max + 1];
+
+        for (int a : A) freq[a]++;
+
+        for (int i = max; i > 0; i--) {
+            long sum = 0, extra = 0;
+            for (int j = i; j <= max; j += i) {
+                sum += freq[j];
+                extra += GCD[j];
             }
-
-            slots--; // Consume one slot
-
-            if (!node.equals("#")) {
-                slots += 2; // Non-null node creates two child slots
-            }
+            GCD[i] = sum * (sum - 1) / 2 - extra;
         }
 
-        return slots == 0;
+        Arrays.parallelPrefix(GCD, Long::sum);
+        int n = queries.length;
+
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            long q = queries[i];
+            int l = 0, h = max + 1;
+            while (l < h) {
+                int m = (l + h) >>> 1;
+                if (GCD[m] <= q) l = m + 1;
+                else h = m;
+            }
+            res[i] = l;
+        }
+
+        return res;
     }
 }
